041a18ef4d3ba73d649254241fe90fd89b9bf97c
Fahad A. Al Khaled <redlock@users.noreply.github.com>
2019-04-16 11:46:04 +0300

    Added SPM

---
+++ Package.swift
@@ -0,0 +1,17 @@
+import PackageDescription
+
+let package = Package(
+    name: "DynamicJSON",
+    products: [
+        .library(name: "DynamicJSON", targets: ["DynamicJSON"]),
+    ],
+    dependencies: [
+     
+    ],
+    targets: [
+        .target(
+            name: "DynamicJSON",
+            dependencies: []),
+
+    ]
+)
